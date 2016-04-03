c76a5a0213ece47ba69693bea9ac515d6e2593d5
Gastón Mulé <gasmul@outlook.com>
2016-04-02 21:45:06 -0300

    isEnAbierta y isEnCerrada

--- src/rubik/busqueda/BusquedaGrafoLCLA.java
+++ src/rubik/busqueda/BusquedaGrafoLCLA.java
@@ -16,62 +16,45 @@ public abstract class BusquedaGrafoLCLA  extends RendimientoBusqueda{
         HashMap<Estado, NodoBusqueda> listaCerrada; 
         LinkedList<NodoBusqueda>      listaAbierta;
 	TrazaGenerica traza;
-/**
- Genera los nodos hijos dado un nodo 
- */
-protected LinkedList<NodoBusqueda> expandirNodo(NodoBusqueda nodoPadre) {
-    LinkedList<NodoBusqueda> expandidos = new LinkedList<NodoBusqueda>(); 
-    for(Operador oper : nodoPadre.getEstado().operadoresAplicables()) {
-      NodoBusqueda n = new NodoBusqueda(nodoPadre.getEstado().aplicarOperador(oper),nodoPadre,oper);
-                        n.setProfundidad(nodoPadre.getProfundidad() + 1); 
-                        n.setCosto(nodoPadre.getCosto() + 1);             
-      if(!listaCerrada.containsKey(n.getEstado())) {
-        expandidos.add(n); 
+    /**
+     Genera los nodos hijos dado un nodo 
+     */
+    protected LinkedList<NodoBusqueda> expandirNodo(NodoBusqueda nodoPadre) {
+        LinkedList<NodoBusqueda> expandidos = new LinkedList<NodoBusqueda>(); 
+        for(Operador oper : nodoPadre.getEstado().operadoresAplicables()) {
+          NodoBusqueda n = new NodoBusqueda(nodoPadre.getEstado().aplicarOperador(oper),nodoPadre,oper);
+                            n.setProfundidad(nodoPadre.getProfundidad() + 1); 
+                            n.setCosto(nodoPadre.getCosto() + 1);             
+          if(!listaCerrada.containsKey(n.getEstado())) {
+            expandidos.add(n); 
+          }
+        }
+         //TRAZA muestro la lista de hijos del nodo padre expandido
+         return expandidos;
       }
+    /**
+     Devuelve el listado de acciones que llegan a la solución encontrada
+     */
+    protected Vector<Operador> encontrarCamino(NodoBusqueda nodoFinal) {
+        Vector<Operador> camino = new Vector<Operador>(); 
+        NodoBusqueda nodoPaso = nodoFinal;
+        while(nodoPaso.getPadre() != null) {
+          camino.insertElementAt(nodoPaso.getOperador(), 0); 
+          nodoPaso = (NodoBusqueda)nodoPaso.getPadre(); 
+        }
+
+        System.out.println("\n REPORTE DE BUSQUEDA");
+        System.out.println("---------------------------------------");
+        //Imprimo Reporte de busqueda antes de retornar el camino para llegar a la solucion
+
+        return camino;
+      }
+
+    public boolean isEnAbierta(NodoBusqueda nodo){
+        return listaAbierta.contains(nodo);
     }
-     //TRAZA muestro la lista de hijos del nodo padre expandido
-     return expandidos;
-  }
-/**
- Devuelve el listado de acciones que llegan a la solución encontrada
- */
-protected Vector<Operador> encontrarCamino(NodoBusqueda nodoFinal) {
-    Vector<Operador> camino = new Vector<Operador>(); 
-    NodoBusqueda nodoPaso = nodoFinal;
-    while(nodoPaso.getPadre() != null) {
-      camino.insertElementAt(nodoPaso.getOperador(), 0); 
-      nodoPaso = (NodoBusqueda)nodoPaso.getPadre(); 
+
+    public boolean isEnCerrada(NodoBusqueda nodo){
+        return listaCerrada.containsKey(nodo.getEstado());
     }
-    
-    System.out.println("\n REPORTE DE BUSQUEDA");
-    System.out.println("---------------------------------------");
-    //Imprimo Reporte de busqueda antes de retornar el camino para llegar a la solucion
-    
-    return camino;
-  }
-
-
-
-    public boolean isEnAbierta(Nodo nodo, LinkedList<Estado> listacerrada){
-    boolean flag=false;
-    for (NodoBusqueda hoja : listaAbierta) {
-    if (nodo.getEstado().equals(hoja.getEstado())) {
-    return true;
-            }
-    }    
-    return flag;
-}
-
-public boolean isEnCerrada(Nodo nodo, LinkedList<Estado> listacerrada){
-    boolean flag=false;
-    for (Estado estado : listacerrada) {
-    if (nodo.getEstado().equals(estado)) {
-    return true;
-            }
-    }    
-    return flag;
-}
-
-
-
 }
