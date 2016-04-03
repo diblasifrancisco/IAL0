e861b2da4b256e4bd57772a681af14a63366e149
Gastón Mulé <gasmul@outlook.com>
2016-04-02 21:59:52 -0300

    tipo busqueda grafo

---
+++ src/rubik/busqueda/TipoBusquedaGrafo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubik.busqueda;
+
+/**
+ *
+ * @author gasmu_000
+ */
+public interface TipoBusquedaGrafo {
+    public static final char CONTROL_LC='A';
+    public static final char CONTROL_LA_LC='B';
+}
